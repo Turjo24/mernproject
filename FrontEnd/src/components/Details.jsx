763665fb772ac2a25f4afff39f81bdd797039d3c
Sharadindu Dutta <turjodutta023@gmail.com>
2024-07-29 23:42:55 +0530

    details page issue

--- FrontEnd/src/components/Details.jsx
+++ FrontEnd/src/components/Details.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { ThreeDots } from 'react-loader-spinner';
-import { toast } from "react-toastify";
+import { productContext } from "../utills/Context";
 
 const Details = () => {
+  const [products, setProducts] = useContext(productContext);
   const [product, setProduct] = useState(null);
   const [quantity, setQuantity] = useState(1);
   const [isEditing, setIsEditing] = useState(false);
@@ -23,12 +23,8 @@ const Details = () => {
   useEffect(() => {
     const fetchUserRole = async () => {
       try {
-        const token = localStorage.getItem('token');
-        if (!token) {
-          setUserRole("guest");
-          return;
-        }
-        const response = await fetch("https://project-cse-2200.vercel.app/api/user/role", {
+        const token = localStorage.getItem('jwtToken');
+        const response = await fetch("https://project-cse-2200-xi.vercel.app/api/user/role", {
           headers: {
             'Authorization': `Bearer ${token}`
           }
@@ -36,12 +32,9 @@ const Details = () => {
         if (response.ok) {
           const data = await response.json();
           setUserRole(data.role);
-        } else {
-          throw new Error("Failed to fetch user role");
         }
       } catch (error) {
         console.error("Error fetching user data:", error);
-        setUserRole("guest");
       }
     };
 
@@ -59,19 +52,16 @@ const Details = () => {
       setLoading(true);
       setError(null);
       try {
-        const token = localStorage.getItem('token');
+        const token = localStorage.getItem('jwtToken');
         const response = await fetch(
-          `https://project-cse-2200.vercel.app/api/products/${id}`,
+          `https://project-cse-2200-xi.vercel.app/api/products/${id}`,
           {
-            headers: token ? {
+            headers: {
               'Authorization': `Bearer ${token}`
-            } : {}
+            }
           }
         );
         if (!response.ok) {
-          if (response.status === 401) {
-            throw new Error("Unauthorized access. Please log in.");
-          }
           const errorData = await response.json();
           throw new Error(errorData.message || "Failed to fetch product");
         }
@@ -81,22 +71,18 @@ const Details = () => {
       } catch (error) {
         console.error("Error fetching product:", error);
         setError(error.message);
-        if (error.message === "Unauthorized access. Please log in.") {
-          toast.error("Please log in to view product details");
-          navigate("/LogInPage");
-        }
       } finally {
         setLoading(false);
       }
     };
     getProduct();
-  }, [id, navigate]);
+  }, [id]);
 
   const ProductDeleteHandler = async () => {
     try {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem('jwtToken');
       const response = await fetch(
-        `https://project-cse-2200.vercel.app/api/products/${id}`,
+        `https://project-cse-2200-xi.vercel.app/api/products/${id}`,
         {
           method: "DELETE",
           headers: {
@@ -108,11 +94,12 @@ const Details = () => {
         const errorData = await response.json();
         throw new Error(errorData.message || "Failed to delete product");
       }
-      toast.success("Product deleted successfully");
+      const updatedProducts = products.filter((p) => p._id !== id);
+      setProducts(updatedProducts);
       navigate("/");
     } catch (error) {
       console.error("Error deleting product:", error);
-      toast.error("Failed to delete product: " + error.message);
+      alert("Failed to delete product: " + error.message);
     }
   };
 
@@ -126,9 +113,9 @@ const Details = () => {
       image: editProduct.image,
     };
     try {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem('jwtToken');
       const response = await fetch(
-        `https://project-cse-2200.vercel.app/api/products/${id}`,
+        `https://project-cse-2200-xi.vercel.app/api/products/${id}`,
         {
           method: "PUT",
           headers: {
@@ -143,27 +130,29 @@ const Details = () => {
         throw new Error(errorData.message || "Failed to update product");
       }
       const data = await response.json();
-      setProduct(data);
+      const updatedProducts = products.map((p) => (p._id === id ? data : p));
+      setProducts(updatedProducts);
       setIsEditing(false);
-      toast.success("Product updated successfully");
+      setProduct(data);
     } catch (error) {
       console.error("Error updating product:", error);
-      toast.error("Failed to update product: " + error.message);
+      alert("Failed to update product: " + error.message);
     }
   };
   
   const handleAddToCart = async () => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('jwtToken');
     if (!token) {
-      toast.error("Please log in to add items to cart");
       navigate("/LogInPage");
       return;
     }
   
     try {
-      const userId = localStorage.getItem('userId');
+      // Assuming the userId is stored in localStorage or can be derived from the token
+      const userId = localStorage.getItem('userId'); // Replace this with your method of obtaining the userId
+  
       const response = await fetch(
-        "https://project-cse-2200.vercel.app/api/cart/add",
+        "https://project-cse-2200-xi.vercel.app/api/cart/add",
         {
           method: "POST",
           headers: {
@@ -171,7 +160,7 @@ const Details = () => {
             'Authorization': `Bearer ${token}`
           },
           body: JSON.stringify({
-            userId,
+            userId,  // Include the userId here
             productId: id,
             quantity,
           }),
@@ -182,172 +171,162 @@ const Details = () => {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
   
-      await response.json();
-      toast.success("Item added to cart successfully");
-      navigate('/CartPage');
+      const result = await response.json();
+      alert("Item added to cart successfully");
+      navigate('/CartPage'); // Navigate to the CartPage after successful addition
     } catch (error) {
       console.error("Error adding product to cart:", error);
-      toast.error(`Failed to add product to cart: ${error.message}`);
+      setError(`Failed to add product to cart: ${error.message}`);
     }
   };
   
+  
   if (loading) {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <ThreeDots 
-          height="80" 
-          width="80" 
-          radius="9"
-          color="#4fa94d" 
-          ariaLabel="three-dots-loading"
-          wrapperStyle={{}}
-          wrapperClassName=""
-          visible={true}
-        />
-      </div>
-    );
+    return <div>Loading...</div>;
   }
 
   if (error) {
-    return <div className="text-center text-red-500 mt-10">{error}</div>;
+    return <div>Error: {error}</div>;
   }
 
   if (!product) {
-    return <div className="text-center mt-10">Product not found</div>;
+    return <div>Product not found</div>;
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen py-12">
-      <div className="container max-w-4xl mx-auto p-4 md:p-10 bg-white shadow-2xl rounded-lg">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-          <img
-            className="object-contain h-64 md:h-96 w-full md:w-1/2 mb-4 md:mb-0 rounded"
-            src={product.image}
-            alt={product.title}
-          />
-          <div className="md:w-1/2 md:ml-10">
-            <h1 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4">
-              {product.title}
-            </h1>
-            <h3 className="text-lg md:text-xl text-gray-500 mb-2 md:mb-4">
-              Category: {product.category}
-            </h3>
-            <h2 className="text-xl md:text-2xl text-red-500 mb-2 md:mb-4">
-              $ {product.price}
-            </h2>
-            <p className="text-base md:text-lg mb-4">{product.description}</p>
-            {isEditing ? (
-              <form onSubmit={ProductEditHandler} className="space-y-4">
-                <div className="mb-4">
-                  <label htmlFor="title" className="block text-sm font-bold text-gray-700">Title</label>
-                  <input
-                    type="text"
-                    id="title"
-                    value={editProduct.title}
-                    onChange={(e) => setEditProduct({...editProduct, title: e.target.value})}
-                    className="w-full p-2 border rounded"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label htmlFor="category" className="block text-sm font-bold text-gray-700">Category</label>
-                  <input
-                    type="text"
-                    id="category"
-                    value={editProduct.category}
-                    onChange={(e) => setEditProduct({...editProduct, category: e.target.value})}
-                    className="w-full p-2 border rounded"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label htmlFor="price" className="block text-sm font-bold text-gray-700">Price</label>
-                  <input
-                    type="number"
-                    id="price"
-                    value={editProduct.price}
-                    onChange={(e) => setEditProduct({...editProduct, price: e.target.value})}
-                    className="w-full p-2 border rounded"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label htmlFor="description" className="block text-sm font-bold text-gray-700">Description</label>
-                  <textarea
-                    id="description"
-                    value={editProduct.description}
-                    onChange={(e) => setEditProduct({...editProduct, description: e.target.value})}
-                    className="w-full p-2 border rounded"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label htmlFor="image" className="block text-sm font-bold text-gray-700">Image URL</label>
-                  <input
-                    type="text"
-                    id="image"
-                    value={editProduct.image}
-                    onChange={(e) => setEditProduct({...editProduct, image: e.target.value})}
-                    className="w-full p-2 border rounded"
-                  />
-                </div>
-                <div className="flex space-x-4">
-                  <button type="submit" className="rounded bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4">
-                    Save
-                  </button>
-                  <button type="button" onClick={() => setIsEditing(false)} className="rounded bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4">
-                    Cancel
-                  </button>
-                </div>
-              </form>
-            ) : (
-              <div className="flex flex-col md:flex-row items-start space-y-4 md:space-x-4 md:space-y-0">
-                {userRole === "admin" ? (
-                  <>
-                    <button
-                      onClick={() => setIsEditing(true)}
-                      className="rounded bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 w-full md:w-auto"
-                    >
-                      Edit
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div className="container w-3/5 mx-auto p-4 md:p-10 flex items-center justify-center shadow-2xl">
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <img
+              className="object-contain h-64 md:h-96 w-full md:w-96 mb-4 md:mb-0"
+              src={product.image}
+              alt={product.title}
+            />
+            <div className="md:ml-10">
+              <h1 className="text-ellipsis text-2xl md:text-4xl font-bold mb-2 md:mb-4">
+                {product.title}
+              </h1>
+              <h3 className="text-lg md:text-xl text-gray-500 mb-2 md:mb-4">
+                Category: {product.category}
+              </h3>
+              <h2 className="text-xl md:text-2xl text-red-500 mb-2 md:mb-4">
+                $ {product.price}
+              </h2>
+              <p className="text-base md:text-lg mb-4">{product.description}</p>
+              {isEditing ? (
+                <form onSubmit={ProductEditHandler} className="space-y-4">
+                  <div className="mb-4">
+                    <label htmlFor="title" className="block text-sm font-bold text-gray-700">Title</label>
+                    <input
+                      type="text"
+                      id="title"
+                      value={editProduct.title}
+                      onChange={(e) => setEditProduct({...editProduct, title: e.target.value})}
+                      className="w-full p-2 border rounded"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="category" className="block text-sm font-bold text-gray-700">Category</label>
+                    <input
+                      type="text"
+                      id="category"
+                      value={editProduct.category}
+                      onChange={(e) => setEditProduct({...editProduct, category: e.target.value})}
+                      className="w-full p-2 border rounded"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="price" className="block text-sm font-bold text-gray-700">Price</label>
+                    <input
+                      type="number"
+                      id="price"
+                      value={editProduct.price}
+                      onChange={(e) => setEditProduct({...editProduct, price: e.target.value})}
+                      className="w-full p-2 border rounded"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="description" className="block text-sm font-bold text-gray-700">Description</label>
+                    <textarea
+                      id="description"
+                      value={editProduct.description}
+                      onChange={(e) => setEditProduct({...editProduct, description: e.target.value})}
+                      className="w-full p-2 border rounded"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="image" className="block text-sm font-bold text-gray-700">Image URL</label>
+                    <input
+                      type="text"
+                      id="image"
+                      value={editProduct.image}
+                      onChange={(e) => setEditProduct({...editProduct, image: e.target.value})}
+                      className="w-full p-2 border rounded"
+                    />
+                  </div>
+                  <div className="flex space-x-4">
+                    <button type="submit" className="rounded bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4">
+                      Save
                     </button>
-                    <button
-                      onClick={ProductDeleteHandler}
-                      className="rounded bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 w-full md:w-auto"
-                    >
-                      Delete
-                    </button>
-                  </>
-                ) : (
-                  <div className="flex items-center space-x-4">
-                    <div className="flex items-center">
-                      <button
-                        onClick={() =>
-                          setQuantity((prev) => (prev > 1 ? prev - 1 : 1))
-                        }
-                        className="rounded-l bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4"
-                      >
-                        -
-                      </button>
-                      <span className="bg-white text-black font-bold py-2 px-4">
-                        {quantity}
-                      </span>
-                      <button
-                        onClick={() => setQuantity((prev) => prev + 1)}
-                        className="rounded-r bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4"
-                      >
-                        +
-                      </button>
-                    </div>
-                    <button
-                      onClick={handleAddToCart}
-                      className="rounded bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 w-full md:w-auto"
-                    >
-                      Add to Cart
+                    <button type="button" onClick={() => setIsEditing(false)} className="rounded bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4">
+                      Cancel
                     </button>
                   </div>
-                )}
-              </div>
-            )}
+                </form>
+              ) : (
+                <div className="flex flex-col md:flex-row items-start space-y-4 md:space-x-4 md:space-y-0">
+                  {userRole === "admin" ? (
+                    <>
+                      <button
+                        onClick={() => setIsEditing(true)}
+                        className="rounded bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 w-full md:w-auto"
+                      >
+                        Edit
+                      </button>
+                      <button
+                        onClick={ProductDeleteHandler}
+                        className="rounded bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 w-full md:w-auto"
+                      >
+                        Delete
+                      </button>
+                    </>
+                  ) : (
+                    <div className="flex items-center space-x-4">
+                      <div className="flex items-center">
+                        <button
+                          onClick={() =>
+                            setQuantity((prev) => (prev > 1 ? prev - 1 : 1))
+                          }
+                          className="rounded-l bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4"
+                        >
+                          -
+                        </button>
+                        <span className="bg-white text-black font-bold py-2 px-4">
+                          {quantity}
+                        </span>
+                        <button
+                          onClick={() => setQuantity((prev) => prev + 1)}
+                          className="rounded-r bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4"
+                        >
+                          +
+                        </button>
+                      </div>
+                      <button
+                        onClick={handleAddToCart}
+                        className="rounded bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 w-full md:w-auto"
+                      >
+                        Add to Cart
+                      </button>
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
